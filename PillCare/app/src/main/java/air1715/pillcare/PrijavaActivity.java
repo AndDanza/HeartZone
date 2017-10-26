b5a0ed3d2488f794c8d908f135aea9f9888e8eef
DESKTOP-R51CPE6\Toni <antbrkic0@foi.hr>
2017-10-26 21:58:01 +0200

    Kreirana je nova aktivnost za registraciju te je klikom na Registracija omogućen prikaz novo kreirane aktivnosti. Također, dodana je slika na početnu aktivnost- prijava.

--- PillCare/app/src/main/java/air1715/pillcare/PrijavaActivity.java
+++ PillCare/app/src/main/java/air1715/pillcare/PrijavaActivity.java
@@ -1,7 +1,10 @@
 package air1715.pillcare;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class PrijavaActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class PrijavaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prijava);
+
+        TextView openRegistrationActivity=(TextView) findViewById(R.id.txt_registracija);
+        openRegistrationActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openActivity=new Intent(PrijavaActivity.this, Registracija_Activity.class);
+                startActivity(openActivity);
+            }
+        });
+
+
     }
+
 }
