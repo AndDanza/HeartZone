4567b6d26dada3e9474c23f0211303ad04ac48cf
Andrea Danzante <anddanzan@foi.hr>
2017-10-29 17:09:05 +0100

    Omogućena funkcionalnost barcode čitača
    Na aktivnosti za pregled lijekova, klikom na tipku skeniraj barcode otvara se čitač koji u obilku Toast obavijesti braća skenirani broj (zasad privremeno)

--- PillCare/app/src/main/java/air1715/pillcare/PopisLijekova_Activity.java
+++ PillCare/app/src/main/java/air1715/pillcare/PopisLijekova_Activity.java
@@ -1,23 +1,35 @@
 package air1715.pillcare;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class PopisLijekova_Activity extends AppCompatActivity {
 
     private DrawerLayout dl;
     private ActionBarDrawerToggle abdt;
+    private Button pokreniBarcodeSkener;
+    private final Activity activity = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_popis_lijekova_);
+
+
         dl= (DrawerLayout) findViewById(R.id.dl);
         abdt = new ActionBarDrawerToggle(this,dl,R.string.Open,R.string.Close);
         abdt.setDrawerIndicatorEnabled(true);
@@ -59,11 +71,47 @@ public class PopisLijekova_Activity extends AppCompatActivity {
                 return true;
             }
         });
+
+
+        pokreniBarcodeSkener = (Button) findViewById(R.id.button_barCodeScanner);
+        //metoda koja pokreće barcode skener
+        pokreniBarcodeSkener.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String prikazUpute = getResources().getString(R.string.scanBarcode);
+
+                IntentIntegrator integrator = new IntentIntegrator(activity);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+                integrator.setPrompt(prikazUpute);
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(false);
+                integrator.setBarcodeImageEnabled(false);
+                integrator.initiateScan();
+            }
+        });
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         return abdt.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Log.d("PopisLijekova_Activity", "Skeniranje prekinuto");
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Log.d("PopisLijekova_Activity", "Skenirano");
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            // This is important, otherwise the result will not be passed to the fragment
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
 
