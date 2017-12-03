398dbaf0dc72c0674330218726aa8ada442041f7
Andrea Danzante <anddanzan@foi.hr>
2017-12-03 18:00:24 +0100

    Nadopunjena klasa za obradu JSON-a
    Klasa HttpUtils unutar paketa Utils za obradu json poziva i kontrolu objekta nadopunjena metodom za obradu JSONArray poziva (prijenos više "redova" unutar jednog json objekta)

--- PillCare/app/src/main/java/air1715/pillcare/Utils/HttpUtils.java
+++ PillCare/app/src/main/java/air1715/pillcare/Utils/HttpUtils.java
@@ -9,6 +9,7 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -61,7 +62,43 @@ public class HttpUtils {
         } catch (IOException e) {
             System.out.println("IOException. " + e.getLocalizedMessage());
         } catch (JSONException e) {
-            System.out.println("JsonException. " + e.getLocalizedMessage());
+            System.out.println("JsonExceptionHTTPUtils. " + e.getLocalizedMessage());
+        }
+        return jsonResult;
+    }
+
+    public static JSONArray sendGetRequestArray(Map<String, Object> params, String path) {
+        Gson gson = new Gson();
+        JSONArray jsonResult = null;
+        try {
+            String paramatersString = "";
+            for (Map.Entry<String, Object> entry : params.entrySet()) {
+                String jsonString;
+                if (!entry.getValue().getClass().equals(String.class))
+                    jsonString = gson.toJson(entry.getValue());
+                else
+                    jsonString = entry.getValue().toString();
+                paramatersString += (URLEncoder.encode(entry.getKey(), "UTF-8") + "="
+                        + URLEncoder.encode(jsonString, "UTF-8")) + "&";
+            }
+            path += "?" + paramatersString.substring(0, paramatersString.length() - 1);
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpGet request = new HttpGet(path);
+            request.addHeader("User-Agent", USER_AGENT);
+            request.addHeader("Accept", "application/json");
+            request.addHeader("Content-Type", "application/json");
+            HttpResponse response = httpClient.execute(request);
+            if (response.getStatusLine().getStatusCode() == HttpURLConnection.HTTP_OK) {
+                HttpEntity entity = response.getEntity();
+                String jsonResponse = EntityUtils.toString(entity);
+                jsonResult = new JSONArray(jsonResponse);
+            }
+        } catch (UnsupportedEncodingException e) {
+            System.out.println("UnsuportedEndodingException. " + e.getLocalizedMessage());
+        } catch (IOException e) {
+            System.out.println("IOException. " + e.getLocalizedMessage());
+        } catch (JSONException e) {
+            System.out.println("JsonExceptionHTTPUtils. " + e.getLocalizedMessage());
         }
         return jsonResult;
     }
