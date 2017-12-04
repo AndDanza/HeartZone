8cf4567663a894fe6bf161c79fe249a8c9d2c1fd
Antonio Brkić <antbrkic0@foi.hr>
2017-12-04 19:41:33 +0100

    Izbrisana je aktivnost za izmjenu podataka te je dodana nova sa pripadajućom klasom

---
+++ PillCare/app/src/main/java/air1715/pillcare/Activities/IzmjenaPodataka_Activity.java
@@ -0,0 +1,15 @@
+package air1715.pillcare.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import air1715.pillcare.R;
+
+public class IzmjenaPodataka_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_izmjena_podataka);
+    }
+}
