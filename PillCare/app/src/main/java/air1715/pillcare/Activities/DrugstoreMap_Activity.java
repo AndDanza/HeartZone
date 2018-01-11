575de2bd37a94f1dc9cc4914a61cde11d0cc2caa
Andrea Danzante <anddanzan@foi.hr>
2018-01-11 20:16:29 +0100

    Dohvaćanje korisnikove lokacije
    Omogućeno dohvaćanje i prikazivanje korisnikove lokacije na google karti

--- PillCare/app/src/main/java/air1715/pillcare/Activities/DrugstoreMap_Activity.java
+++ PillCare/app/src/main/java/air1715/pillcare/Activities/DrugstoreMap_Activity.java
@@ -1,15 +1,13 @@
 package air1715.pillcare.Activities;
 
-import android.support.v4.app.FragmentActivity;
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 import air1715.pillcare.R;
 
@@ -40,10 +38,6 @@ public class DrugstoreMap_Activity extends AppCompatActivity implements OnMapRea
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.setMyLocationEnabled(true);
     }
 }
